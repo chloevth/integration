ad9b89b0784150b0bb9bd80632613a58b829f889
acs <c.vauthier@codeur.online>
2021-03-24 16:59:50 +0100

    integration

---
+++ integration/index.php
@@ -0,0 +1,93 @@
+<?php include 'header.php';?>
+
+
+<div class="container">
+
+<!-- début de la navigation -->
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="ui-kit/logo.png" id="logo">
+    </a>
+   
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Sell</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Marketplace</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Community</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Develop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resources</a>
+        </li>
+      
+      </ul>
+      <a href="#" id="log">Log In</a>
+      <form class="d-flex">
+        <button class="btn btn-outline-success" id="search-btn" type="submit">Get Started</button>
+      </form>
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+      </svg>
+    </div>
+  </div>
+</nav>
+
+<!-- fin de la navigation -->
+
+<!-- début section 1 -->
+
+  <div class="row row-1">
+
+
+    <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-sm">Col 2
+        
+    </div>
+     
+
+
+
+
+</div>
+
+<!-- fin section 1 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
