c8d6bacc224f8ad33eabc61657689bae7b40e221
acs <c.vauthier@codeur.online>
2021-03-25 16:40:21 +0100

    updat :)

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intégration
+Intégration maquette mobile téléchargée sur Figma, conçue par un webdesigner professionel. touch
---
+++ index.php
@@ -0,0 +1,218 @@
+<?php include 'header.php';?>
+
+
+<!-- début section 1 -->
+<section id="section-1">
+<div class="container">
+
+<!-- début de la navigation -->
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="ui-kit/logo.png" id="logo">
+    </a>
+   
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Sell</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Marketplace</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Community</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Develop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resources</a>
+        </li>
+      
+      </ul>
+      <a href="#" id="log">Log In</a>
+      <form class="d-flex">
+        <button class="btn btn-outline-success " id="search-btn" type="submit">Get Started</button>
+      </form>
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+      </svg>
+    </div>
+  </div>
+</nav>
+
+<!-- fin de la navigation -->
+
+
+<!-- ligne 1-->
+  <div class="row">
+
+<!-- col 1 -->
+    <div class="col-sm">
+      <div class="card mx-auto" style="width: 18rem;">
+        <div class="card-body">
+          <h1 class="card-title">Building exactly the eCommerce website you want.</h1>
+         
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button type="button" class="s1-btn btn btn-primary rounded-pill">Start a New Store</button>
+          <a href="#" class="card-link">or Customize and Extend</a>
+        </div>
+      </div>
+    </div>
+
+
+<!-- col 2 -->
+    <div class="col-sm">
+      <img src="ui-kit/product.png" class="img-fluid mx-right" alt="...">
+    </div>
+     
+
+
+
+
+</div>
+</section>
+<!-- fin section 1 -->
+
+
+<!-- début section 2 -->
+
+<section class="section-2">
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+
+  
+
+</section>
+
+<!-- fin section 2 -->
+
+<section class="section-3">
+  <div class="container-fluid">
+
+  <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  </div>
+</section>
+
+
+
+<section class="section-4">
+
+
+
+
+
+
+
+
+
+</section>
+
+
+
+<section class="section-5">
+  <div class="container">
+    <div class="row">
+      <h2>Supported by real people</h2>
+      <p>Our team of Happiness Engineers works remotely from 58 countries providing customer support across multiple time zones.</p>
+      <img src="ui-kit/team.png" alt="" srcset="">
+    </div>
+
+  </div>
+
+
+</section>
+
+<section class="section-6 text-white">
+  <div class="container h-100">
+    <div class="row align-items-center h-100 justify-content-center ">
+      <p class="col-md-8">WooCommerce - <strong>the most customizable eCommerce platform </strong> for building <strong>your online business.</strong></p>
+      <button type="button" class="cta-btn btn btn-primary col-6 col-md-2 rounded-pill border-white border-2">Get Started</button>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
